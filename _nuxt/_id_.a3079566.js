1bb0a4080fcd736f7675c306d5fb2e417105835e
Nadiein Oleh <olegnadein1994@gmail.com>
2023-10-20 18:42:12 +0300

    Updates

--- _nuxt/_id_.a3079566.js
+++
@@ -1,1 +0,0 @@
-import{M as o,c as s,t as a,P as t,o as r}from"./entry.fe61d456.js";const n={class:"flex-grow-1"},_={__name:"[id]",setup(i){return o(),(e,c)=>(r(),s("main",n," event id: "+a((e._.provides[t]||e.$route).params.id),1))}};export{_ as default};
---
+++ assets/_id_.8b29d0ed.js
@@ -0,0 +1,1 @@
+import{M as o,c as s,t as a,P as t,o as r}from"./entry.18d23527.js";const n={class:"flex-grow-1"},_={__name:"[id]",setup(i){return o(),(e,c)=>(r(),s("main",n," event id: "+a((e._.provides[t]||e.$route).params.id),1))}};export{_ as default};
