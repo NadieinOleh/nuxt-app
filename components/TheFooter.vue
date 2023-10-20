d8cdb3481f5b292e9a686d3c10dcb2550894dd04
Nadiein Oleh <olegnadein1994@gmail.com>
2023-10-20 17:55:19 +0300

    fixed routes

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="footer">
     <nav class="navbar custom-bg p-4 d-flex gap-5 justify-content-start">
-      <NuxtLink active-class="active" class="custom-link" to="/contacs">social Links</NuxtLink>
-      <NuxtLink active-class="active" class="custom-link" to="/social align-self-center">social Links</NuxtLink>
+      <NuxtLink active-class="active" class="custom-link" to="/">social Links</NuxtLink>
+      <NuxtLink active-class="active" class="custom-link" to="/">social Links</NuxtLink>
     </nav>
   </footer>
 </template>
